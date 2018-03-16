2ac6ab99f66dcf5c9a92aacc5b7c73306e096686
Eric Choo <eric.choo1997@yahoo.com>
2018-03-16 09:22:33 +0800

    add test

---
+++ src/setupTests.js
@@ -0,0 +1,11 @@
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+}
+global.localStorage = localStorageMock
+
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
